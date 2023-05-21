0fd9bbad9a84d8d60bcb1be6b0c69b427a67f92b
PrimeGlorious <TheComboPunk@gmail.com>
2023-05-21 14:03:25 +0300

    finished

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import API from './fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onInputText, DEBOUNCE_DELAY));
+
+function onInputText(e) {
+  if (!e.target.value.trim()) {
+    return (countryListReset());
+  }
+
+  API.fetchCountries(e.target.value.trim())
+    .then(countries => renderCountriesList(countries))
+    .catch(error => console.log(error), countryListReset());
+}
+
+function renderCountriesList(countries) {
+  countryListReset();
+
+  if (countries.length > 10) {
+    return Notiflix.Notify.info(
+      `"Too many matches found. Please enter a more specific name."`
+    );
+  }
+  const markup = countries
+    .map(country => {
+        return `<li>
+        <div style="display:flex; align-items:center"><div style="display:inline-flex; align-items:center"><img src="${country.flags.svg}" alt="flag" width = 40></div>
+        <h1 class="title"; style="font-size:20px; padding-left:10px", "line-height:1.5">${country.name.official}</h1></div>
+        </li>`;
+    })
+    .join('');
+  refs.countryList.insertAdjacentHTML('beforeend', markup);
+
+  const markupInfo = countries.map(country => {
+    if (countries.length === 1) {
+      const title = document.querySelector('.title');
+      title.style.fontSize = '40px';
+       return `<li>
+        <p><b>Capital:</b> ${country.capital}</p>
+      </li>
+      <li>
+        <p><b>Population:</b> ${country.population}</p>
+      </li>
+      <li>
+        <p><b>Languages:</b> ${Object.values(country.languages)}</p>
+      </li>`;
+    }
+  }).join('');
+   refs.countryInfo.insertAdjacentHTML('beforeend', markupInfo);
+}
+
+function countryListReset() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+}
